6870a9dd77c23d4c2f9bd1a459bcd338ddde92df
Alexandre Alves Veloso <alexandre.aveloso@dinfor.uniceub>
2024-10-10 09:18:16 -0300

    origin

--- API_Node/server.js
+++ API_Node/server.js
@@ -15,3 +15,11 @@ app.use(express.json()) // permitir o envio de dados no formato JSON
 
 
 mongoose.connect('mongodb://localhost/produtos', {useUrlParser: true})
+
+//definir o schema para produtos
+const produtosSchema = new mongoose.Schema({
+    nome: String,
+    quantidade: Number,
+    imagem: String,
+    preco: Number
+})
