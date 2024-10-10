bd243bda7e9000b7c6055296ec7e7af1779958f5
Alexandre Alves Veloso <alexandre.aveloso@dinfor.uniceub>
2024-10-10 09:12:38 -0300

    primeiro

---
+++ API_Node/server.js
@@ -0,0 +1,17 @@
+//framework web para node.js
+const express = require('express')
+
+//biblioteca para trabalhar com mongodb
+const mongoose = require('mongoose')
+
+//cors "meio campo" para permitir requisições 
+// entre diferentes dominios (hosts / servidos)
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors()) // premitir que o front acesse o API
+app.use(express.json()) // permitir o envio de dados no formato JSON
+
+
+mongoose.connect('mongodb://localhost/produtos', {useUrlParser: true})
