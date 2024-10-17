9cf4eb90f31f0a7b20859626bf84af93cf27d1d1
Alexandre Alves Veloso <alexandre.aveloso@dinfor.uniceub>
2024-10-17 10:39:08 -0300

    finalizando a aula

--- Back-end/sistema_web/pp.py
+++ Back-end/sistema_web/pp.py
@@ -1,14 +1,15 @@
-from flask import Flask 
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Word!"
+    user = "Alexandre Veloso"
+    return render_template('index.html', user = user)
 
 @app.route('/sobre')
 def sobre():
-    return 'pagina sobre'
+    return render_template('sobre.html')
 
 if __name__ == '__main__':
     app.run(port = 8000)
